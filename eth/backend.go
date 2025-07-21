d9107903e1c02d7ed4314bbc0ddf249d4b0ef216
Zhang Guyu <zhangguyu6@gmail.com>
2025-07-21 09:42:19 +0000

    fix

--- eth/backend.go
+++ eth/backend.go
@@ -451,6 +451,9 @@ func (s *Ethereum) APIs() []rpc.API {
 		}, {
 			Namespace: "net",
 			Service:   s.netRPCService,
+		}, {
+			Namespace: "trace",
+			Service:   NewDebankAPI(s),
 		},
 	}...)
 }
