21a7c5e34006a66b2c9631d07515e096ce6ac428
0byteme <819643718@qq.com>
2026-04-07 15:47:24 +0800

    feat: support pipeline tracer for celo l2 (#52)
    * feat: impl pipeline tracer
    
    fix ci
    
    * fix ci
    
    * fix trace genesis block
    
    * fix celo tx types
    
    * add log
    
    * fix tx baseFee

--- core/state_processor.go
+++ core/state_processor.go
@@ -147,7 +147,23 @@ func ApplyTransactionWithEVM(msg *Message, gp *GasPool, statedb *state.StateDB, 
 			hooks.OnTxStart(evm.GetVMContext(), tx, msg.From)
 		}
 		if hooks.OnTxEnd != nil {
-			defer func() { hooks.OnTxEnd(receipt, err) }()
+			defer func() {
+				var baseFee = evm.Context.BaseFee
+				if receipt.BaseFee != nil {
+					baseFee = receipt.BaseFee
+				}
+				receipt.SetEffectiveGasPrice(tx, baseFee)
+				var l1Fee *big.Int
+				if evm.Context.L1CostFunc != nil && !tx.IsDepositTx() && receipt.GasUsed > 0 {
+					l1Fee = evm.Context.L1CostFunc(tx.RollupCostData(), evm.Context.Time)
+					if l1Fee != nil && l1Fee.Cmp(common.Big0) > 0 {
+						gasUsed := new(big.Int).SetUint64(receipt.GasUsed)
+						receipt.EffectiveGasPrice = new(big.Int).Div(new(big.Int).Add(l1Fee, new(big.Int).Mul(receipt.EffectiveGasPrice, gasUsed)), gasUsed)
+					}
+				}
+				log.Info("OnTxEnd", "txHash", tx.Hash().Hex(), "baseFee", evm.Context.BaseFee, "effectiveGasPrice", receipt.EffectiveGasPrice, "l1Fee", l1Fee)
+				hooks.OnTxEnd(receipt, err)
+			}()
 		}
 	}
 
