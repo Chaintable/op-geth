799111c1608e8eee691feb0b8947eccba8d7b6f8
chendechen <dechen.chen@debank.com>
2026-08-25 10:57:51 +0800

    core: guard optional pipeline globals

--- core/blockchain.go
+++ core/blockchain.go
@@ -4058,12 +4058,19 @@ func (bc *BlockChain) writeBlockAndSetHead(block *types.Block, receipts []*types
 	// 先确保 pipeline tracer 不为空，然后再判断是否需要push kafka
 	// 上一个push kafka的block, 必然存在(至少有genesis block)
 	// 上一个push kafka的block比当前的head block还要新，说明有unwind回退，不需要处理, 即使是fork，等有更新的block的时候再一起push
-	isLeader := leader.GlobalManager.IsLeader()
-	leader.GlobalManager.RLock()
-	lastPushedBlock := tracer.NodeXPusher.LastPushedBlock()
-	leader.GlobalManager.RUnlock()
+	manager, nodeXPusher := leader.GlobalManager, tracer.NodeXPusher
+	var (
+		isLeader        bool
+		lastPushedBlock *ptypes.BlockContext
+	)
+	if manager != nil && nodeXPusher != nil {
+		isLeader = manager.IsLeader()
+		manager.RLock()
+		lastPushedBlock = nodeXPusher.LastPushedBlock()
+		manager.RUnlock()
+	}
 
-	if tracer.NodeXPusher != nil && isLeader && lastPushedBlock.BlockNumber <= block.NumberU64() {
+	if nodeXPusher != nil && isLeader && lastPushedBlock != nil && lastPushedBlock.BlockNumber <= block.NumberU64() {
 		_, dropBlocks, newBlocks := bc.getCommonAncestor(*lastPushedBlock, ptypes.BlockContext{
 			BlockNumber: block.NumberU64(),
 			Hash:        block.Hash(),
@@ -4091,7 +4098,7 @@ func (bc *BlockChain) writeBlockAndSetHead(block *types.Block, receipts []*types
 		}
 
 		if blockChange != nil {
-			err := tracer.NodeXPusher.PushBlockChangeNotification(blockChange)
+			err := nodeXPusher.PushBlockChangeNotification(blockChange)
 			if err != nil {
 				log.Error("SetCanonical PushBlockChangeNotification error", "err", err)
 			}
@@ -5211,12 +5218,19 @@ func (bc *BlockChain) SetCanonical(head *types.Block) (common.Hash, error) {
 	// 先确保 pipeline tracer 不为空，然后再判断是否需要push kafka
 	// 上一个push kafka的block, 必然存在(至少有genesis block)
 	// 上一个push kafka的block比当前的head block还要新，说明有unwind回退，不需要处理, 即使是fork，等有更新的block的时候再一起push
-	isLeader := leader.GlobalManager.IsLeader()
-	leader.GlobalManager.RLock()
-	lastPushedBlock := tracer.NodeXPusher.LastPushedBlock()
-	leader.GlobalManager.RUnlock()
+	manager, nodeXPusher := leader.GlobalManager, tracer.NodeXPusher
+	var (
+		isLeader        bool
+		lastPushedBlock *ptypes.BlockContext
+	)
+	if manager != nil && nodeXPusher != nil {
+		isLeader = manager.IsLeader()
+		manager.RLock()
+		lastPushedBlock = nodeXPusher.LastPushedBlock()
+		manager.RUnlock()
+	}
 
-	if tracer.NodeXPusher != nil && isLeader && lastPushedBlock.BlockNumber <= head.NumberU64() {
+	if nodeXPusher != nil && isLeader && lastPushedBlock != nil && lastPushedBlock.BlockNumber <= head.NumberU64() {
 		_, dropBlocks, newBlocks := bc.getCommonAncestor(*lastPushedBlock, ptypes.BlockContext{
 			BlockNumber: head.NumberU64(),
 			Hash:        head.Hash(),
@@ -5244,7 +5258,7 @@ func (bc *BlockChain) SetCanonical(head *types.Block) (common.Hash, error) {
 		}
 
 		if blockChange != nil {
-			err := tracer.NodeXPusher.PushBlockChangeNotification(blockChange)
+			err := nodeXPusher.PushBlockChangeNotification(blockChange)
 			if err != nil {
 				log.Error("SetCanonical PushBlockChangeNotification error", "err", err)
 			}
